a43d5235031fe35f3c54730e101e9398f0bc2158
Nate Gowins <ngowins@gmail.com>
2022-04-05 20:08:30 -0500

    Completed exercise

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+  let userWatchingLiveStream = true;
+
+  if (userWatchingLiveStream == true) {
+    resolve("Please like and subscribe!");
+  } else {
+    reject("User has left.");
+  }
+});
+
+watching
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
